992a3542400344d61faca46860ec65b5e5937138
Subr1ata <subrotomukherjee8@gmail.com>
2023-12-24 16:32:02 +0530

    setting hero section

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-24 lg:flex lg:h-screen">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            TubeGuruji Marketplace <br></br>
+            Access Free &nbsp;
+            <strong className="font-extrabold text-primary sm:block">
+              Source Code & Assets
+            </strong>
+          </h1>
+
+          <p className="mt-4 sm:text-xl/relaxed">
+            Now get free source code and assets for all project with membership
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+              href="/get-started"
+            >
+              Get Started
+            </a>
+
+            <a
+              className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-blue-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+              href="/about"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
-  return <>Home</>;
+  return <Hero />;
 }
